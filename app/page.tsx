a993c6ff80a69910daf593b2fca01b6061342938
Marco Nucara <marco.nucara@gmail.com>
2024-03-20 14:47:34 +0700

    feat: change homepage font sizes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-white hover:text-zinc-300"
+              className="text-sm duration-500 text-zinc-300 hover:text-zinc-400"
             >
               {item.name}
             </Link>
@@ -31,7 +31,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-white">
+        <h2 className="text-sm text-zinc-300 lg:text-xl">
           Building Stronger Teams, Driving Innovation Through Optimized
           Engineering Management
         </h2>
