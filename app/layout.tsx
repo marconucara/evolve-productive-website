7b14aa9825cdbcb8e663d74d0d427e9aab22c2a2
Marco Nucara <marco.nucara@gmail.com>
2024-03-20 14:44:07 +0700

    feat: add background gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${
+        className={` ${
           process.env.NODE_ENV === "development" ? "debug-screens" : undefined
         }`}
       >
